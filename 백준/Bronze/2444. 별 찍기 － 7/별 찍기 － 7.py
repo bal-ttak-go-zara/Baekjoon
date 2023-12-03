fd3c87de7adfa364c88645c9c479c929962f9d7d
bal-ttak-go-zara <97681639+bal-ttak-go-zara@users.noreply.github.com>
2023-12-03 21:03:25 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(1, n+1) :
+    print(' ' * (n-i), end='')
+    print('*' * (2*i-1))
+    
+for j in range(1, n) :
+    print(' ' * j, end='')
+    print('*' * (2*(n-j)-1))
