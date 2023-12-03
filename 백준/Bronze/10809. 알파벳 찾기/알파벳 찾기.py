540079f96e31ef574b299222495a5fe91502c74a
bal-ttak-go-zara <97681639+bal-ttak-go-zara@users.noreply.github.com>
2023-12-03 21:06:33 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,8 @@
+word = list(input())
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for a in alphabet :
+    if a in word :
+        print(word.index(a), end=' ')
+    else :
+        print('-1', end=' ')
